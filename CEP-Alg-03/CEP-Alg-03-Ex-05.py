4780b43387c3c6b3e24f3056e782e89887731fdd
Eduardo <carloseduardoperetiatko07@gmail.com>
2026-03-23 09:47:43 -0300

    up

--- CEP-Alg-03/CEP-Alg-03-Ex-05.py
+++ CEP-Alg-03/CEP-Alg-03-Ex-05.py
@@ -3,3 +3,21 @@
 # um mês (como uma string). Então seu programa deve exibir uma mensagem informando a
 # quantidade de dias daquele mês. Caso o mês seja fevereiro, sua mensagem pode informar
 # “28 ou 29 dias”.
+
+
+#achei necessario criar um vetor para validação, para nao fazer um macth case gigante
+validos = ["janeiro", "fevereiro", "marco", "abril", "maio", "junho", 
+         "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"]
+
+mes = input("Informe um mes para descobrir quantos dias ele terá: ")
+mes = mes.strip().lower()
+
+#diminui e remove espaco
+
+if mes in validos:
+    if mes == "fevereiro":
+        print("28 ou 29 dias!")
+    else:
+        print("30 ou 31 dias !!")
+else:
+    print("Mes inválido!\nOu verifique sua ortografia...")
